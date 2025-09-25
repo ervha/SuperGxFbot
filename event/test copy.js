b9a38b84c83cacc56a0307f1cb6998a0e8834b65
Oervha <reiun1130@gmail.com>
2025-09-26 01:12:14 +0900

    v1.1

--- event/test copy.js
+++ event/test copy.js
@@ -1,9 +1,13 @@
-const { Events, MessageFlags } = require('discord.js');
+const { Events } = require('discord.js');
+const { prefix } = require('../config.json');
 
 module.exports = {
 	name: Events.MessageCreate,
 	async execute(message) {
         if (message.author.bot || message.webhookId) return;
-        await message.reply(message.content);
+		if (!message.content.startsWith(prefix)) return;
+		if (message.content.includes('post')){
+        	await message.react('<:3587_20250619175247:1420699809790033930>');
+		}
 	},
 };
--- event/test.js
+++ event/test.js
@@ -1,9 +1,11 @@
-const { Events, MessageFlags } = require('discord.js');
+const { Events } = require('discord.js');
+const { prefix } = require('../config.json');
 
 module.exports = {
 	name: Events.MessageCreate,
 	async execute(message) {
         if (message.author.bot || message.webhookId) return;
+		if (message.content.startsWith(prefix)) return;
         await message.reply(message.content);
 	},
 };
