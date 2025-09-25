e35c829daa90609dbaae9edb0abf9be8671f7bcf
Oervha <reiun1130@gmail.com>
2025-09-25 21:09:31 +0900

    微調整

--- commands/utility/help.js
+++ commands/utility/help.js
@@ -5,14 +5,18 @@ module.exports = {
 		.setName('help')
 		.setDescription('botに関するヘルプを表示します。'),
 
-    const: embed = new EmbedBuilder()
+	async execute(interaction) {
+        const embed = new EmbedBuilder()
 	    .setColor(0x0099FF)
         .setTitle('commands-list')
-        .setDescription('プレフィックス:', congig.prefix, '\n\n★サーバーのコマンド\n詳細はコマンド入力時の説明を参照')
-        .addFields("")
-	    .setTimestamp(),
-    
-	async execute(interaction) {
+        .setDescription('\n\n★サーバーのコマンド\n詳細はコマンド入力時の説明を参照')
+        .addFields(
+            { name: '/help', value: 'botに関するヘルプを表示します。' },
+            { name: '/ping', value: 'Botの応答速度を確認します。' },
+            { name: '/server', value: 'サーバーの情報を表示します。' },
+            { name: '/user', value: 'ユーザーの情報を表示します。' },
+        )
+	    .setTimestamp()
 
 		await interaction.reply({ embeds: [embed] });
 	},
