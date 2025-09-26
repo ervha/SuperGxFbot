4018553152a7c897d2eefb33acdbc18c00b0fdda
Oervha <reiun1130@gmail.com>
2025-09-27 03:42:51 +0900

    v2.01

--- event/delete-react.js
+++ event/delete-react.js
@@ -3,16 +3,16 @@ module.exports = {
     description: 'リアクションでメッセージを削除する',
     async execute(client,command,args,message){
         if (message.content.includes('delete')){
-            const sent = await message.reply('<:3587_20250619175247:1420699809790033930>');
-            const reaction = await sent.react('❌')
+            const sent = await message.channel.send('<:3587_20250619175247:1420699809790033930>');
+            const reaction = await sent.react('❌');
             
-            const collectorfilter = (reaction, user) => {
-                return reaction.emoji.name === '❌' && user.id === interaction.user.id;
+            const Filter = (reaction, user) => {
+                return reaction.emoji.name === '❌' && user.id === message.author.id;
             };
 
-            sent.awaitReactions({ filter: collectorfilter, max: 1, time: 5000, errors: ['time'] })
+            sent.awaitReactions({ filter: Filter, max: 1, time: 5000, errors: ['time'] })
                 .then(() => sent.delete()) // リアクションされたら送信したメッセージを削除する
-                .catch(() => reaction.remove()) // リアクションされなかったら自身で付けたリアクションを消す(必須ではない)
+                .catch(() => reaction.remove()); // リアクションされなかったら自身で付けたリアクションを消す(必須ではない)
         }
     },
 };
--- event/test.js
+++ event/test.js
@@ -2,6 +2,7 @@ module.exports = {
 	name: "test",
 	description: 'メッセージをそのまま返す',
     async execute(client,command,args,message){
-        await message.reply(message.content);
+        await message.channel.send(message.content);
+		await message.delete();
 	},
 };
