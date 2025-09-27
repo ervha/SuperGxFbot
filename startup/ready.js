c51982f82ac0acd513aa357917e54f82eed503c5
Oervha <reiun1130@gmail.com>
2025-09-27 21:10:36 +0900

    v2.4

--- startup/ready.js
+++ startup/ready.js
@@ -1,10 +1,24 @@
 const { Events } = require('discord.js');
+const { logging_channel_id } = require('../config.json');
 
 module.exports = {
 	name: Events.ClientReady,
 	once: true,
-	execute(client) {
-		client.user.setActivity('hi!', { type: 'PLAYING' })
-		console.log(`Ready! Logged in as ${client.user.tag}`);
+	async execute(client) {
+		await client.user.setActivity('hi!', { type: 'PLAYING' })
+
+		await console.log(`Ready! Logged in as ${client.user.tag}`);
+
+		try {
+			const loggingChannel = client.channels.cache.get(logging_channel_id);
+
+			if (loggingChannel) {
+				await loggingChannel.send(`✅ Botを起動しました)`);
+			} else {
+				console.error(`設定されたロギングチャンネルID (${logging_channel_id}) が見つかりません。`);
+			}
+		} catch (error) {
+			console.error('起動メッセージの送信中にエラーが発生しました:', error);
+		}
 	},
 };
