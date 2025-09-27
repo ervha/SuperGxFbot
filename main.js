bd183c8e5843e102e051a8a8c0b3bde936c3aa57
Oervha <reiun1130@gmail.com>
2025-09-27 18:54:20 +0900

    v2.21

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const fs = require('node:fs');
 const path = require('node:path');
-const { Client, Collection, GatewayIntentBits, user, MessageReaction, EmbedBuilder } = require('discord.js');
+const { Client, Collection, GatewayIntentBits } = require('discord.js');
 const { token, prefix } = require('./config.json');
 const client = new Client({
     intents: [
