a173f2dcb82de174a8298968c56fa2e83a17e339
Oervha <reiun1130@gmail.com>
2025-09-27 22:17:48 +0900

    v2.4

--- startup/ready.js
+++ startup/ready.js
@@ -8,17 +8,18 @@ module.exports = {
 		await client.user.setActivity('hi!', { type: 'PLAYING' })
 
 		await console.log(`Ready! Logged in as ${client.user.tag}`);
-
+/**
 		try {
 			const loggingChannel = client.channels.cache.get(logging_channel_id);
 
 			if (loggingChannel) {
-				await loggingChannel.send(`✅ Botを起動しました)`);
+				await loggingChannel.send(`✅ Botを起動しました`);
 			} else {
 				console.error(`設定されたロギングチャンネルID (${logging_channel_id}) が見つかりません。`);
 			}
 		} catch (error) {
 			console.error('起動メッセージの送信中にエラーが発生しました:', error);
 		}
+*/
 	},
 };
