4229cdaf66963bb913c3818eaf685154bb84a86e
ervha <reiun1130@gmail.com>
2026-05-15 11:49:10 +0900

    v2.4.1 初期化処理の追加と微調整

--- startup/ready.js
+++ startup/ready.js
@@ -1,5 +1,7 @@
 const { Events } = require('discord.js');
 const { logging_channel_id } = require('../config.json');
+const fs = require('fs').promises;
+const path = require('path');
 
 module.exports = {
 	name: Events.ClientReady,
@@ -11,6 +13,11 @@ module.exports = {
 
 		try {
 			const loggingChannel = client.channels.cache.get(logging_channel_id);
+
+			const filePath = path.join(__dirname, '../commands/event/txt/room.txt')		
+			const dir = path.dirname(filePath, 'utf8');
+			await fs.mkdir(dir, { recursive: true });
+			await fs.writeFile(filePath, '', 'utf8')
 
 			if (loggingChannel) {
 				await loggingChannel.send(`✅ Botを起動しました`);
